68f71feee2b14cdb0d0f58db0313094d039bff6b
Cyriaque MALDAT <cmaldat@gmail.com>
2017-06-07 09:48:13 +0200

    Suppression : Duration, releaseDateCine, note Ajout : Price, Buykey in entity Media Création entity : note

---
+++ src/AppBundle/Entity/Note.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Note
+ *
+ * @ORM\Table(name="note")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\NoteRepository")
+ */
+class Note
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_users", referencedColumnName="id")
+     * })
+     */
+    private $idUsers;
+
+    /**
+     * @var int
+     *
+     * @ORM\ManyToOne(targetEntity="Media")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_media", referencedColumnName="id_media")
+     * })
+     */
+    private $idMedia;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="name", type="float", nullable=false)
+     */
+    private $note;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set idUsers
+     *
+     * @param integer $idUsers
+     *
+     * @return Note
+     */
+    public function setIdUsers($idUsers)
+    {
+        $this->idUsers = $idUsers;
+
+        return $this;
+    }
+
+    /**
+     * Get idUsers
+     *
+     * @return int
+     */
+    public function getIdUsers()
+    {
+        return $this->idUsers;
+    }
+
+    /**
+     * Set idMedia
+     *
+     * @param string $idMedia
+     *
+     * @return Note
+     */
+    public function setIdMedia($idMedia)
+    {
+        $this->idMedia = $idMedia;
+
+        return $this;
+    }
+
+    /**
+     * Get idMedia
+     *
+     * @return string
+     */
+    public function getIdMedia()
+    {
+        return $this->idMedia;
+    }
+}
+
--- src/AppBundle/Form/RegistrationType.php
+++ src/AppBundle/Form/RegistrationType.php
@@ -2,13 +2,18 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('name');
+        $builder
+            ->add('description',TextareaType::class,array(
+                'label' => 'Description'
+            ))
+            ->add('name');
     }
 
     public function getParent()
