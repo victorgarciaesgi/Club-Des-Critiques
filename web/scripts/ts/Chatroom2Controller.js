a9790556fd5b93dc0678ea27f8478e30d09dc03b
Victor Garcia <victorgarciaparis13@gmail.com>
2017-06-15 20:31:46 +0200

    Pages d'information du salon finie (oups j'étais sur master), plus qu'a remplir de données

--- web/scripts/ts/Chatroom2Controller.js
+++ web/scripts/ts/Chatroom2Controller.js
@@ -5,7 +5,6 @@ MainApp.controller('chatroom2', function ($scope, $rootScope, AjaxRequest, momen
   $scope.Chatroom = {
     salons: {
       elements: [],
-      selectedSalon: {id: 1},
       loading: true,
       select(salon){
         // click sur un salon
@@ -29,29 +28,49 @@ MainApp.controller('chatroom2', function ($scope, $rootScope, AjaxRequest, momen
     filter: {
       title: ""
     },
+    selectedSalon: {},
     infos:{
       open: true,
       details: false,
       users: [],
       book: {},
     },// Contient toutes les infos du salon selectionné
+    options: [
+      {text: 'Inviter un contact dans ce salon', action:'invite', icon:'add_contact'},
+      {text: 'Signaler un contenu inapproprié', action:'reportSalon', icon:'signaler'},
+    ],
+    execute(method){
+      this[method]();
+    },
+    invite(){
+      console.log('invite');
+    },
+    reportSalon(){
+      console.log('report:' + this.selectedSalon.id)
+    },
     scroll(){
-      $('.messages-container').scrollTop(90000)
+      var container = $('#messages-container');
+      if (container[0].scrollHeight > container.innerHeight()){
+        container.scrollTop(container.innerHeight());
+      }
+
     },
     init(){
       AjaxRequest.get('library_getOneBook', 3).then((result) => {
         this.infos.book = result;
+        this.selectedSalon = this.salons.elements[0];
       })
     }
   }
 
   $scope.Chatroom.init();
 
-  $scope.userID = 1; // a deplacer dans un $rootscope, recuperer par le controlleur
+  $scope.userID = 1; // a deplacer dans un $rootscope, recuperé par le controlleur
 
-  for (var i = 0; i < 20; i++) {
+  for (var i = 0; i < 8; i++) {
     $scope.Chatroom.salons.elements.push({id: i + 1, title: loremIpsum()})
     $scope.Chatroom.messages.elements.push({id: i + 1, user: randomNumber(1, 4), text: loremIpsum()});
+    $scope.Chatroom.infos.users.push({id: i + 1, name: chance.name()});
   }
 
   $scope.$watch('Chatroom.messages.elements', function(newValue, oldValue, scope){
