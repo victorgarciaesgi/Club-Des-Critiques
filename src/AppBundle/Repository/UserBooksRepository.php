0b92dd7d4754b6a4976bc9198b92c7416f37f2ae
mcyriaque <cmaldat@gmail.com>
2017-07-16 01:01:48 +0200

    Envoie message vers un user + profile interface

---
+++ src/AppBundle/Repository/UserBooksRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Cyriaque
+ * Date: 15/07/2017
+ * Time: 17:09
+ */
+
+namespace AppBundle\Repository;
+
+
+class UserBooksRepository extends \Doctrine\ORM\EntityRepository
+{
+
+}
